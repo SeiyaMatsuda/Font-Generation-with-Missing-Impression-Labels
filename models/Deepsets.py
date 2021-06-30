ba97d5f1f3c1f15cc5d0113a1670da71dd9b9b75
SeiyaMatsuda <63505312+SeiyaMatsuda@users.noreply.github.com>
2021-06-30 18:54:16 +0900

    マルチラベル学習に変更 CPGAN機構は用いない

--- models/Deepsets.py
+++ models/Deepsets.py
@@ -48,10 +48,9 @@ class DeepSets(nn.Module):
         #     x = x[mask]
         #     x = x.reshape(1, -1, 300)
         phi_output = self.phi(x)
-        sum_output = phi_output.mean(1)
+        # sum_output = phi_output.mean(1)
+        sum_output = torch.cat([ph[((x_!=0).sum(1)>0)].mean(0).view(1, -1) for x_, ph in zip(x, phi_output)], dim=0)
         rho_output = self.rho(sum_output)
-        #     rho_outputs.append(rho_output)
-        # rho_outputs = torch.cat(rho_outputs, dim=0)
         return rho_output
 
 if __name__ == '__main__':
